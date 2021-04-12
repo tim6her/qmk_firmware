2531e9ed1b6c916f66fcbb22a65310bba7cc3007
Tim B. Herbsrith <tim.herbstrith@icloud.com>
2021-04-12 09:47:02 +0200

    Adding layers to make the keyboard usable
    :100644 100644 71a77dabe 1fffba9f3 M	keyboards/crkbd/keymaps/tim6her-neo2/keymap.c
    
    :100644 100644 937d67e25 76c2a44bc M	keyboards/crkbd/keymaps/tim6her-neo2/config.h
    :100644 100644 71a77dabe f60f43ee7 M	keyboards/crkbd/keymaps/tim6her-neo2/keymap.c
    :100644 100644 1d7dc095b 9444b88d5 M	keyboards/crkbd/keymaps/tim6her-neo2/rules.mk

--- keyboards/crkbd/keymaps/tim6her-neo2/keymap.c
+++ keyboards/crkbd/keymaps/tim6her-neo2/keymap.c
@@ -22,25 +22,41 @@ enum custom_keycodes {
   MY_MOD3 = SAFE_RANGE
 };
 
-enum {
-  TD_SS_BSPC = 0
-};
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_SS_BSPC] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_BSPC)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      LCTL_T(KC_TAB), KC_Q, KC_W,   KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  TD(TD_SS_BSPC),
+      LCTL_T(KC_TAB), KC_Q, KC_W,   KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_QUOTE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, MY_MOD3,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, RGUI_T(KC_Z), KC_X,  KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, RGUI_T(KC_SLSH),  KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LCTL, KC_LGUI, KC_ENT,        KC_SPC, KC_RGUI, KC_RALT
+                                        KC_LCTL, KC_LGUI, LT(2, KC_ENT),        LT(1, KC_SPC), KC_LALT, KC_RALT
                                       //`--------------------------'  `--------------------------'
+  ),
 
+  [1] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+         KC_TAB,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_BSPC,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_CAPS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_CAPS,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_LSFT, KC_RGUI, KC_RBRC,  KC_EQL, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI,  KC_RSFT,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LCTL, KC_LGUI, KC_ENT,        _______, KC_LALT, KC_RALT
+                                        //`--------------------------'  `--------------------------'
+  ),
+
+  [2] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLUP, XXXXXXX, XXXXXXX,                    XXXXXXX, KC_BSPC,   KC_UP, KC_DEL, XXXXXXX, XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLDOWN, XXXXXXX, XXXXXXX,                  XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX, XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, XXXXXXX, XXXXXXX, KC__MUTE, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LCTL, KC_LGUI, _______,        KC_SPC, KC_LALT, KC_RALT
+                                        //`--------------------------'  `--------------------------'
   )
 };
 
@@ -153,7 +169,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         unregister_code(KC_CAPS); // Change the key that was held here, too!
         if (timer_elapsed(my_mod3_timer) < TAPPING_TERM) {
-          SEND_STRING("\'"); // Change the character(s) to be sent on tap here
+          SEND_STRING("\["); // Change the character(s) to be sent on tap here
         }
       }
       return false; // We handled this keypress
